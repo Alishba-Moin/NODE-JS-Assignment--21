427a07185df8288f74d5f927c44a74d803a4c5d2
Alishba-Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-02-21 06:12:48 -0800

    Add files via upload

---
+++ q21.js
@@ -0,0 +1,9 @@
+"use strict";
+// 21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let Person = {
+    name: "ALISHBA",
+    fname: "MOIN",
+    age: 18,
+    rollno: 150
+};
+console.log(person);
---
+++ q21.ts
@@ -0,0 +1,16 @@
+// 21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+type obj = {
+    name:string,
+    fname:string,
+    age:number,
+    rollno:number 
+}
+
+let Person = {
+    name : "ALISHBA",
+    fname : "MOIN",
+    age : 18,
+    rollno : 150
+}
+console.log(Person)
